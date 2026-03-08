ab13396c3fdfe69f4718e380476c5a7bb71e0d38
Shivam6-m <sc0712744@gmail.com>
2026-03-08 13:08:04 +0530

    update vector4

--- veda/vector4.cpp
+++ veda/vector4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     vector<char> vec={'a','b','c','d','e','d','f'};
-    for(char val: vec){
+    for(char val: vec){// char
         cout<<val<<endl;
     }
 return 0;
