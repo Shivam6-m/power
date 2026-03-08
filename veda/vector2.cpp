b2991ff92af4ec66abfef09b3ba05c94add488f9
Shivam6-m <sc0712744@gmail.com>
2026-03-08 13:01:26 +0530

    added vector programs 1 to 6

---
+++ veda/vector2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec={1,2,3,4};
+   
+    cout<<vec[2]<<endl;
+return 0;
+}//this is 3 size of vector if we want to print and loactio means index is print that particular loaction like 
+// if we print 0 it print 1
