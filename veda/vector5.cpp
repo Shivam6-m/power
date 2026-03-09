fac892f6fe5865eadbd5d82b4fb466a73a3140ea
Shivam6-m <sc0712744@gmail.com>
2026-03-09 23:48:18 +0530

    update vector5

--- veda/vector5.cpp
+++ veda/vector5.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    vector<char> vec={'a','b','c','d','f'};
+    vector<char> vec={'a','b','c','d','f'};//char
     cout<<"size="<<vec.size()<<endl;// ohk 
     for(char val: vec){
         cout<<val<<endl;
