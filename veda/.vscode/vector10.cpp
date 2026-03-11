5f8beba31e399094539c70bdad358c1208088a8c
Shivam6-m <sc0712744@gmail.com>
2026-03-11 21:10:48 +0530

    update vector10

--- veda/.vscode/vector10.cpp
+++ veda/.vscode/vector10.cpp
@@ -4,7 +4,7 @@ int main(){
     int n = 5;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++)
-            cout<<" ";
+            cout<<" ";//99
         for(int j=1;j<=2*i-1;j++)
             cout<<"*";
         cout<<endl;
