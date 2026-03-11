2576b563aebd96c8fa0a4ce66b6a182fd1ac7b02
Shivam6-m <sc0712744@gmail.com>
2026-03-11 21:08:56 +0530

    update vector8

--- veda/.vscode/vector8.cpp
+++ veda/.vscode/vector8.cpp
@@ -4,7 +4,7 @@ int main(){
     int n=5;
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=n-i;j++)
+        for(int j=1;j<=n-i;j++)//
         cout<<" ";
         if(i==1)
         cout<<"O";
