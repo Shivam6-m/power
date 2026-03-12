079f7b9006b59ad29219b15750aa9eae9b611861
Shivam6-m <sc0712744@gmail.com>
2026-03-12 19:48:50 +0530

    Added kadane1

--- veda/.vscode/kadane1.cpp
+++ veda/.vscode/kadane1.cpp
@@ -4,7 +4,7 @@ int main()
 // today we solve array question with the help of kadane algorthiims 
 {
 int n;
-cout<<" enter the n number -";
+cout<<" enter the n number -";//
 cin>>n;
 int arr[5]={1,2,3,4,5};
 for(int st=0; st<n;st++){
