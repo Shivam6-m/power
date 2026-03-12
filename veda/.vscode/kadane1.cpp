8cbd3f26cc0f10ba47f18efa72de2500156e76cd
Shivam6-m <sc0712744@gmail.com>
2026-03-12 19:46:29 +0530

    Added kadane1 subarray program

---
+++ veda/.vscode/kadane1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+// today we solve array question with the help of kadane algorthiims 
+{
+int n;
+cout<<" enter the n number -";
+cin>>n;
+int arr[5]={1,2,3,4,5};
+for(int st=0; st<n;st++){
+    for(int end=st; end<n; end++){
+        for(int i=st; i<=end;i++){
+            cout<<arr[i];
+        }
+        cout<<" ";
+    }
+    cout<<endl;
+}
+return 0;
+}
