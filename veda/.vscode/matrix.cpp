122ea9ab15e6ee33ce9109fa9289a9e7077b1dcf
Shivam6-m <sc0712744@gmail.com>
2026-03-13 20:28:56 +0530

    add matrix cpp

---
+++ veda/.vscode/matrix.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>a(n,vector<int>(m));
+    vector<vector<int>>b(n,vector<int>(m));
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>b[i][j];
+        }
+    }
+    vector<vector<int>>c(n,vector<int>(m));
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>c[i][j];
+        }
+    }
+return 0;
+}
