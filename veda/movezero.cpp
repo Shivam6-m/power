70557935cfd0ec42074a8ed2787da5597ac46c74
Shivam6-m <sc0712744@gmail.com>
2026-03-14 21:05:30 +0530

    add movezero.cpp

---
+++ veda/movezero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void moveZeroes(vector<int>& nums){
+    int left = 0;
+    for(int right = 0; right < nums.size(); right++){
+        if(nums[right] != 0){
+            swap(nums[left], nums[right]);
+            left++;
+        }
+    }
+}
+int main(){
+    vector<int> nums = {0,1,0,3,12};
+    moveZeroes(nums);
+    for(int x : nums){
+        cout << x << " ";
+    }
+    return 0;
+}
