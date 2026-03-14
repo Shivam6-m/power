498ceb5c4c130a24f8fa7dfb97c3aa7b1ccd758c
Shivam6-m <sc0712744@gmail.com>
2026-03-14 21:03:32 +0530

    add twosum.cpp

---
+++ veda/twosum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int left = 0;
+    int right = numbers.size() - 1;
+    while(left < right){
+        int sum = numbers[left] + numbers[right];
+
+        if(sum == target){
+            return {left + 1, right + 1};
+        }
+        else if(sum < target){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+    return {};
+}
+int main(){
+    vector<int> numbers = {2,7,11,15};
+    int target = 9;
+    vector<int> result = twoSum(numbers, target);
+    cout << "Indices: ";
+    for(int x : result){
+        cout << x << " ";
+    }
+    return 0;
+}
