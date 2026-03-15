f339799b22cc2f7cc737c55235a7ef736067cfa6
Shivam6-m <sc0712744@gmail.com>
2026-03-15 20:33:11 +0530

    add twosum.cpp

--- veda/twosum.cpp
+++ veda/twosum.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 vector<int> twoSum(vector<int>& numbers, int target) {
-    int left = 0;
+    int left = 0;//
     int right = numbers.size() - 1;
     while(left < right){//ohk
         int sum = numbers[left] + numbers[right];
