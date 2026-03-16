e622b11975fbee909cd5bbf26b8170678524701a
Shivam6-m <sc0712744@gmail.com>
2026-03-16 18:37:28 +0530

    add am2.cpp

--- veda/am2.cpp
+++ veda/am2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n,m,k,s;
     cin>>n>>m>>k>>s;
-    char park[100][100];
+    char park[100][100];//char
     for(int i = 0; i < n; i++){
         for(int j=0;j<m;j++){
             cin >> park[i][j];
