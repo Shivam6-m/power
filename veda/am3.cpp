ec7636877ac597084270d67e420ff56542208dcf
Shivam6-m <sc0712744@gmail.com>
2026-03-16 18:37:08 +0530

    add am3.cpp

--- veda/am3.cpp
+++ veda/am3.cpp
@@ -4,7 +4,7 @@ int main(){
     int n;
     cin>>n;
     int arr[100][100];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){//
         for(int j=0;j<n;j++){
             cin>>arr[i][j];
         }
