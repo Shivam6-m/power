b84be031ae685e06274f31e0338358f19e57f4e3
Shivam6-m <sc0712744@gmail.com>
2026-03-18 04:38:50 +0530

    bookallocation.cpp

--- veda/.vscode/bookallocation.cpp
+++ veda/.vscode/bookallocation.cpp
@@ -25,7 +25,7 @@ int allocateBooks(vector<int> &arr, int n, int m){
         return -1;
     }
    int sum=0;
-    for(int i=0;i <n ;i++){ // 0(n)
+    for(int i=0;i <n ;i++){ // 0(n) ohk
         sum +=arr[i];
     } 
 
