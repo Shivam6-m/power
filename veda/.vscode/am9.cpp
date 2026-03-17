5a32930965c9b4e6b7a6ac3f3ff144ea086abbc3
Shivam6-m <sc0712744@gmail.com>
2026-03-17 18:23:42 +0530

    add am8.cpp

---
+++ veda/.vscode/am9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[1000][1000];
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++) {
+        for(int j=i;j<n;j++) {
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+    for(int j=0;j<n;j++) {
+        int top=0,bottom=n-1;
+        while(top<bottom){
+            swap(arr[top][j],arr[bottom][j]);
+            top++;
+            bottom--;
+        }
+    }
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cout<<arr[i][j]<< " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
