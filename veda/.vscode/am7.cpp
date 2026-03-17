13fca92b2afe32c2b7280bd095dc644d223ec61c
Shivam6-m <sc0712744@gmail.com>
2026-03-17 18:07:47 +0530

    add am7.cpp

--- veda/.vscode/am7.cpp
+++ veda/.vscode/am7.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int m,n;
     cin>>m>>n;
-    int arr[100][100];
+    int arr[100][100];//
     for(int i=0;i<m;i++){
         for(int j = 0;j<n;j++){//
             cin>>arr[i][j];
