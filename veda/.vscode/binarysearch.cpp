9a2675a95394367cafee3027f2a555937893d6c4
Shivam6-m <sc0712744@gmail.com>
2026-03-20 04:45:11 +0530

    “binarysearch.cpp git push origin main '/Users/shivam/Desktop/power/veda/.vscode/binarysearch.cpp'git add . git commit -m “binarysearch.cpp

---
+++ veda/.vscode/binarysearch.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarysearch(vector<int> arr, int target){
+    int start=0;
+    int end=arr.size()-1;
+    while(start <=end){
+       int mid= start +(start -end)/2;
+        if( target > arr[mid]){
+            start=mid +1;
+        }
+        else if ( target <arr[mid]){
+            end = mid -1;
+        }
+        else {
+            return mid;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> arr1= {-1,0,3,4,5,9,12};// for odd
+    int target1=12;
+ 
+     //cout<<binarysearch(arr1, target1) <<endl;
+
+
+    vector<int> arr2 ={-1,0,3,5,9,12};//for even 
+    int target2=10;
+  cout<<binarysearch(arr2, target2) <<endl;
+vector<int> arr3 ={3,4,5,6,7,0,1,2,};
+int target3=0;
+ //cout<<binarysearch(arr3, target3) <<endl;
+return 0;
+}
