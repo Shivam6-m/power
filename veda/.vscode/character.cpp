b15d4546efd8a5b60958cfc4fd66f4e9ef138fc4
Shivam6-m <sc0712744@gmail.com>
2026-03-21 05:42:37 +0530

    “character.cpp git push origin main '/Users/shivam/Desktop/power/veda/.vscode/character.cpp'git add . git commit -m “character.cpp

---
+++ veda/.vscode/character.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char str[]= "shivam";// string literals means the valu are same 
+cout<< str[2]<<endl;
+return 0;
+}
---
+++ veda/.vscode/stringgg.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>// its a optional 
+using namespace std;
+int main()
+{
+    string  str = " SHIVAM";// string is dynamic => runtime mein resize 
+    str ="yes shivam bolo";
+cout << str<<endl ;
+
+return 0;
+}
