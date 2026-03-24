8bb4e5ffb35213c10d60945dbec6c4d5cb5bb208
Shivam6-m <sc0712744@gmail.com>
2026-03-24 11:35:02 +0530

    “da2.cpp git push origin main '/Users/shivam/Desktop/power/veda/da2.cpp'git add . git commit -m “da2.cpp

--- veda/tempCodeRunnerFile.cpp
+++ veda/tempCodeRunnerFile.cpp
@@ -1,18 +1,4 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-int n,i;
-cin>>n;
-for(int i=1;i<=n;i++){
-    for(int j=1;j<i+1;j++){ 
-    cout<<"* ";
-}
-cout<<endl;
-}
-
-
-
-
-return 0;
-}
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
