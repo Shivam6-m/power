c4cc22bfebaf005bffe9be743ecbaaca23236c9d
Shivam6-m <sc0712744@gmail.com>
2026-03-29 22:20:05 +0530

    “da2.cpp git push origin main '/Users/shivam/Desktop/power/veda/da2.cpp'git add . git commit -m “da2.cpp

--- veda/da2.cpp
+++ veda/da2.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-string removeOccurrences(string s, string part) {
+string removeOccurrences(string s, string part){
     while (s.find(part) != string::npos) {//
         s.erase(s.find(part), part.length());//
     }
