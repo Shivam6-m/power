44dad1aea9414a989540f7189dde8d7a5f89dded
Shivam6-m <sc0712744@gmail.com>
2026-04-01 10:31:19 +0530

    add am1.cpp

--- veda/am1.cpp
+++ veda/am1.cpp
@@ -4,7 +4,7 @@ int main(){
     int n,m;
     cin>>n>>m;
     int arr[30][30];
-    for(int i = 0;i<n; i++){
+    for(int i = 0;i<n; i++){//
         for(int j=0;j<m;j++){
             cin>>arr[i][j];
         }
