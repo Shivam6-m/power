69152e087a18ae7810ec7de580449770c0c5f4e2
Shivam6-m <sc0712744@gmail.com>
2026-04-01 10:28:58 +0530

    add am9.cpp

--- veda/.vscode/am9.cpp
+++ veda/.vscode/am9.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int arr[1000][1000];
+    int arr[1000][1000];//
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {//
             cin>>arr[i][j];
