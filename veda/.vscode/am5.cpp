7eff264030635f102f6ff440ca0db1bef9addf5a
Shivam6-m <sc0712744@gmail.com>
2026-04-06 02:11:27 +0530

    add am5.cpp

--- veda/.vscode/am5.cpp
+++ veda/.vscode/am5.cpp
@@ -4,7 +4,7 @@ int main(){
     int m,n;//
     cin>>m>>n;
     int arr[10][10];//
-    for(int i=0;i<m;i++){
+    for(int i=0;i<m;i++){//
         for(int j=0;j<n;j++){//////
             cin>>arr[i][j];
         }
