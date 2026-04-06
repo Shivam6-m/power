51bbeb5796d93291bb3b39fa20f5fe6792ed3193
Shivam6-m <sc0712744@gmail.com>
2026-04-06 09:09:56 +0530

    add am8.cpp

--- veda/.vscode/am8.cpp
+++ veda/.vscode/am8.cpp
@@ -6,7 +6,7 @@ int main(){//
     int arr[1000][1000];////////
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {////
-            cin>>arr[i][j];
+            cin>>arr[i][j];//
         }
     }
     for(int i=0;i<n;i++) {
