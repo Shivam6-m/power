d2d4d99db91207291a04807c9499da1e000a6d55
Shivam6-m <sc0712744@gmail.com>
2026-04-07 20:05:19 +0530

    add am9.cpp

--- veda/.vscode/am9.cpp
+++ veda/.vscode/am9.cpp
@@ -13,7 +13,7 @@ int main(){
         for(int j=i;j<n;j++) {//
             swap(arr[i][j],arr[j][i]);
         }//
-    }
+    }//
     for(int j=0;j<n;j++) {
         int top=0,bottom=n-1;
         while(top<bottom){
