9c0e052c279f6cab124d2d8baac25eba30e80ea0
Shivam6-m <sc0712744@gmail.com>
2026-04-07 20:05:35 +0530

    add am9.cpp

--- veda/.vscode/am9.cpp
+++ veda/.vscode/am9.cpp
@@ -21,7 +21,7 @@ int main(){
             top++;//
             bottom--;
         }
-    }
+    }//
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {//
             cout<<arr[i][j]<< " ";
