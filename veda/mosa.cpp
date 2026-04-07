5ae3c08afc00c00a070fcd22bf9cbb2ade4b21cd
Shivam6-m <sc0712744@gmail.com>
2026-04-07 20:09:43 +0530

    “mosa.cpp git push origin main '/Users/shivam/Desktop/power/veda/mosa.cpp'git add . git commit -m “mosa.cpp

--- veda/mosa.cpp
+++ veda/mosa.cpp
@@ -5,7 +5,7 @@ int main()
     int n=5;
     int arr[5]={1,2,3 ,4,5};
     int maxsum=INT_MIN;
-    for (int start =0; start<n;start++){
+    for (int start =0; start<n;start++){//
         int currsum=0;
         for (int end = start ; end< n; end++){
             currsum +=arr[end];
